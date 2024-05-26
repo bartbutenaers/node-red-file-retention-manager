f9fe2d77436d9c1acb2d0553de02d59b889092c0
bartbutenaers <bart.butenaers@gmail.com>
2024-05-26 08:05:36 +0200

    Major refactoring

--- cleanup_fs.js
+++ cleanup_fs.js
@@ -1,7 +1,9 @@
 module.exports = function(RED) {
     const fs = require('fs').promises
     const path = require('path')
+    const globToRegExp = require('glob-to-regexp')
 
+// TODO perhaps rename to file-retention-manager??
     function cleanupFilesystem(config) {
         RED.nodes.createNode(this, config)
         let node = this
@@ -26,24 +28,21 @@ module.exports = function(RED) {
             this.status({fill:'blue', shape:'dot', text:'processing'})
 
             let baseFolder = config.baseFolder
-            let fileNamePattern = config.fileNamePattern
-            let folderNamePattern = config.folderNamePattern
+            let patternType = config.patternType
             let age = config.age
             let ageUnit = config.ageUnit
             let removeEmptyFolders = config.removeEmptyFolders
             let dryRun = config.dryRun
             let report = config.report
+            let patterns = config.patterns
 
             // All the config properties can be overwritten by input msg properties
             if (msg.payload && typeof msg.payload === 'object') {
                 if (msg.payload.baseFolder !== undefined) {
                     baseFolder = msg.payload.baseFolder
                 }
-                if (msg.payload.fileNamePattern !== undefined) {
-                    fileNamePattern = msg.payload.fileNamePattern
-                }
-                if (msg.payload.folderNamePattern !== undefined) {
-                    folderNamePattern = msg.payload.folderNamePattern
+                if (msg.payload.patternType !== undefined) {
+                    patternType = msg.payload.patternType
                 }
                 if (msg.payload.age !== undefined) {
                     age = msg.payload.age
@@ -59,6 +58,9 @@ module.exports = function(RED) {
                 }
                 if (msg.payload.report !== undefined) {
                     report = msg.payload.report
+                }
+                if (msg.payload.patterns !== undefined || msg.payload.pattern.length == 0) {
+                    patterns = msg.payload.patterns
                 }
             }
 
@@ -77,15 +79,16 @@ module.exports = function(RED) {
                 resetStatus()
                 return
             }
+            if (patterns === undefined || !Array.isArray(patterns) || patterns.length == 0) {
+                node.error("Specify 'payload.patterns' when not specified in the config screen.")
+                resetStatus()
+                return
+            }
 
-            // Remove ';' at the start or the end of the patterns, to avoid empty strings in the pattern arrays
-            fileNamePattern = fileNamePattern.replace(/^;|;$/g, "")
-            folderNamePattern = folderNamePattern.replace(/^;|;$/g, "")
-
-            // Convert the ';' separated strings of patterns to arrays.
-            // When no patterns are supplied, then all files/folders will be removed
-            fileNamePattern = fileNamePattern.split(';') || ['.*']
-            folderNamePattern = folderNamePattern.split(';') || ['.*']
+            // Convert glob patterns to regex expressions if patternType is 'glob'
+            if (patternType === 'glob') {
+                patterns = patterns.map(globToRegExp)
+            }
 
             let deletedFiles = 0
             let deletedFolders = 0
@@ -103,31 +106,6 @@ module.exports = function(RED) {
                 'years': age * 60 * 60 * 24 * 365
             }[ageUnit]
 
-            async function deleteIfOld(filePath) {
-                try {
-                    const stats = await fs.stat(filePath)
-                    const now = new Date().getTime()
-                    const fileAge = (now - stats.mtime) / 1000
-
-                    if (fileAge > ageInSeconds && fileNamePattern.some(pattern => new RegExp(pattern).test(path.basename(filePath)))) {
-                        if (report) {
-                            reportContent.files.push(filePath)
-                        }
-
-                        if (!dryRun) {
-                            try {
-                                await fs.unlink(filePath)
-                                deletedFiles++
-                            } catch (err) {
-                                node.error(err)
-                            }
-                        }
-                    }
-                } catch (err) {
-                    node.error(err)
-                }
-            }
-
             async function walkDir(dir) {
                 try {
                     const files = await fs.readdir(dir)
@@ -141,24 +119,37 @@ module.exports = function(RED) {
                                 // Do a dept-first traversal, i.e. always descend into directories to allow full path filtering
                                 await walkDir(filePath)
 
-                                // Check the regex of the full folder name
-                                if (folderNamePattern.some(pattern => new RegExp(pattern).test(filePath))) {
-                                    // Check if the folder is empty considering also the files that would have been deleted
-                                    const isEmpty = (await fs.readdir(filePath)).every(file => reportContent.files.includes(path.join(filePath, file)))
-                                    if (removeEmptyFolders && isEmpty) {
-                                        if(report) {
-                                            reportContent.folders.push(filePath)
-                                        }
+                                // Check if the folder is empty considering also the files that would have been deleted
+                                const isEmpty = (await fs.readdir(filePath)).every(file => reportContent.files.includes(path.join(filePath, file)))
+                                if (removeEmptyFolders && isEmpty) {
+                                    reportContent.folders.push(filePath)
 
-                                        if (!dryRun) {
-                                            await fs.rmdir(filePath)
-                                            deletedFolders++
-                                        }
+                                    if (!dryRun) {
+                                        await fs.rmdir(filePath)
+                                        deletedFolders++
                                     }
                                 }
-                            } else if (folderNamePattern.some(pattern => new RegExp(pattern).test(path.dirname(filePath)))) {
-                                // Only delete files if their parent directory matches the folderNamePattern
-                                await deleteIfOld(filePath)
+                            } else {
+                                // Only delete files if their parent directory matches the patterns
+                                try {
+                                    const now = new Date().getTime()
+                                    const fileAge = (now - stats.mtime) / 1000
+
+                                    if (fileAge > ageInSeconds && patterns.some(pattern => pattern.test(filePath))) {
+                                        reportContent.files.push(filePath)
+
+                                        if (!dryRun) {
+                                            try {
+                                                await fs.unlink(filePath)
+                                                deletedFiles++
+                                            } catch (err) {
+                                                node.error(err)
+                                            }
+                                        }
+                                    }
+                                } catch (err) {
+                                    node.error(err)
+                                }
                             }
                         } catch (err) {
                             node.error(err)
@@ -173,8 +164,10 @@ module.exports = function(RED) {
 
             msg.payload = {
                 deletedFiles: deletedFiles,
-                deletedFolders: deletedFolders,
-                report: reportContent
+                deletedFolders: deletedFolders
+            }
+            if (report) {
+                msg.payload.report = reportContent
             }
             node.send(msg)
 
